fc602fe8e5deaf2e73bfab2331613a18cd95ae70
Said Rodrigues <coderflemis@gmail.com>
2024-06-02 08:03:07 -0300

    update flake.nix to include protobuf

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
             (rust-bin.stable.latest.default.override {
               extensions = [ "rust-src" "rust-analyzer-preview" ];
             })
+            protobuf
           ];
           shellHook = "";
         };
